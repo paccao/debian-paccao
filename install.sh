4200547605d6717264ce908288f9ac72e315a0f5
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-12-10 15:50:43 +0100

    update paths

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ fi
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list
 
-cd ~/dotfiles_/Debian
-
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
@@ -36,40 +34,40 @@ cp -R Backgrounds /home/$username/Pictures/
 mv user-dirs.dirs /home/$username/.config
 chown -R $username:$username /home/$username
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
-mv $builddir/.config/sddm.conf /etc/sddm.conf
+mv /home/$username/debian-paccao/.config/sddm.conf /etc/sddm.conf
 
 ## Installing sugar-candy dependencies
-#nala install libqt5svg5 qml-module-qtquick-controls #qml-module-qtquick-controls2 -y
-## Installing Essential Programs
-#nala install feh i3 sxhkd urxvt rofi picom thunar nitrogen #lxpolkit x11-xserver-utils unzip yad wget pulseaudio pulseeffects #pavucontrol vim tmux -y
-## Installing Other less important Programs
-#nala install neofetch flameshot lxappearance papirus-icon-theme #fonts-noto-color-emoji fonts-font-awesome sddm -y
-#
-## Installing fonts
-#cd $builddir
-#nala install fonts-font-awesome
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.#1.0/FiraCode.zip
-#unzip FiraCode.zip -d /home/$username/.fonts
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.#1.0/Meslo.zip
-#unzip Meslo.zip -d /home/$username/.fonts
-#mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
-#chown $username:$username /home/$username/.fonts/*
-#
-## Reloading Font
-#fc-cache -vf
-## Removing zip Files
-#rm ./FiraCode.zip ./Meslo.zip
-#
-## Install brave-browser
-#sudo nala install apt-transport-https curl -y
-#sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.#gpg https://brave-browser-apt-release.s3.brave.com/#brave-browser-archive-keyring.gpg
-#echo "deb [signed-by=/usr/share/keyrings/#brave-browser-archive-keyring.gpg arch=amd64] https://#brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/#apt/sources.list.d/brave-browser-release.list
-#sudo nala update
-#sudo nala install brave-browser -y
-#
-## Enable graphical login and change target from CLI to GUI
-#systemctl enable sddm
-#systemctl set-default graphical.target
-#
-#echo "Installation complete. Now reboot your system with:"
-#echo "sudo reboot"
+nala install libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
+# Installing Essential Programs
+nala install feh i3 sxhkd urxvt rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pulseeffects pavucontrol vim tmux -y
+# Installing Other less important Programs
+nala install neofetch flameshot lxappearance papirus-icon-theme fonts-noto-color-emoji fonts-font-awesome sddm -y
+
+# Installing fonts
+cd $builddir
+nala install fonts-font-awesome
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install brave-browser
+sudo nala install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/rave-browser-archive-keyring.#gpg https://rave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /tc/#apt/sources.list.d/brave-browser-release.list
+sudo nala update
+sudo nala install brave-browser -y
+
+# Enable graphical login and change target from CLI to GUI
+systemctl enable sddm
+systemctl set-default graphical.target
+
+echo "Installation complete. Now reboot your system with:"
+echo "sudo reboot"
