1e86ecb26ced3e6079f034c51a6a6c0b6b760f43
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-12-11 12:09:17 +0100

    move post-install.sh into install.sh

--- install.sh
+++ install.sh
@@ -63,5 +63,23 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 sudo nala update
 sudo nala install --no-install-recommends brave-browser -y
 
+# Create custom ls alias
+echo "alias l='ls -AlhF'" >> /home/$username/.bashrc
+echo "alias g='git'" >> /home/$username/.bashrc
+echo "alias gp='git pull'" >> /home/$username/.bashrc
+echo "alias gs='git status'" >> /home/$username/.bashrc
+echo "alias ga='git add .'" >> /home/$username/.bashrc
+echo "alias gf='git fetch'" >> /home/$username/.bashrc
+
+# Autostart programs
+cp /etc/X11/xinit/xinitrc /home/$username/.xinitrc
+echo "exec xclip &
+exec i3" >> /home/$username/.xinitrc
+
+cd ~ &&
+source .bashrc &&
+source .bash_profile &&
+source .xinitrc
+
 echo "Installation complete. Now reboot your system with:"
 echo "sudo reboot"
