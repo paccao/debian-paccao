c465fe5795efa5cbcf6bcc5952ee73bd222f9843
paccao <16489683+paccao@users.noreply.github.com>
2023-04-28 17:42:02 +0200

    improve update script

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cd ~
-cp -a .config/i3/ debian-paccao/dotconfig/
-cp .Xresources debian-paccao/ 
-cp .Xmodmap debian-paccao/
-cp .vimrc debian-paccao/
-cp .tmux.conf debian-paccao/
-cp -a .screenlayout debian-paccao/
-cp .xinitrc debian-paccao/
-cp update-dotfiles.sh debian-paccao/
-cp .bashrc debian-paccao/
-cp .bash_aliases debian-paccao/
+echo "This script assumes the path to the script is ~/update-dotfiles.sh"
+sleep 1
+
+cp -a ~/.config/i3/ ~/debian-paccao/dotconfig/
+cp ~/.Xresources ~/debian-paccao/ 
+cp ~/.Xmodmap ~/debian-paccao/
+cp ~/.vimrc ~/debian-paccao/
+cp ~/.tmux.conf ~/debian-paccao/
+cp ~/update-dotfiles.sh ~/debian-paccao/
+cp ~/.bashrc ~/debian-paccao/
+cp ~/.bash_aliases ~/debian-paccao/
